4718a969ac77aa6c1b05006c4664df9e15318f62
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2019-08-28 14:15:32 +0200

    Jordans main menu
    Main menu update

---
+++ Combat Arena/Assets/Scripts Jordan/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
