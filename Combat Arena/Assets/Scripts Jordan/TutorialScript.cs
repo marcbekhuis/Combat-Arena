3255a3c59e8512b3eb873215b17813722dc62626
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2019-08-29 10:18:37 +0200

    Tutorial + main menu
    Tutorial is tijdelijk done en main menu werkt (tutorial button alleen en de quit)

---
+++ Combat Arena/Assets/Scripts Jordan/TutorialScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject text1;
+    public GameObject text2;
+    public static int text;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        text2.SetActive(false);
+        text = 0;
+    }
+
+    private void Update()
+    {
+        Debug.Log(text);
+    }
+
+    public void TutorialNext ()
+    {
+        Debug.Log("buttonpressed " + text);
+        if (text == 0)
+        {
+            text1.SetActive(false);
+            text += 1;
+            text2.SetActive(true);
+        }
+        /*else if (text == 1)
+        {
+            text2.SetActive(false);
+            text = 2;
+            text3.SetActive(true);
+        }*/
+    }
+}
---
+++ Combat Arena/Assets/Scripts Jordan/TutorialScript1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TutorialScript1 : MonoBehaviour
+{
+    public GameObject text1;
+    public GameObject text2;
+
+    public void TutorialBack()
+    {
+        Debug.Log("buttonpressedBack" + TutorialScript.text);
+        /*if (text == 2)
+        {
+            text3.SetActive(false);
+            text = 1;
+            text2.SetActive(true);
+        }*/
+        if (TutorialScript.text == 1)
+        {
+            text2.SetActive(false);
+            TutorialScript.text = 0;
+            text1.SetActive(true);
+        }
+        else if (TutorialScript.text == 0)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
