15997d7f68135587f3a6d0646cc7d76e52bce464
marc <marc@bekhuis.net>
2019-08-30 11:52:11 +0200

    jump fixed, input fixed

--- Combat Arena/Assets/Scripts/PlayerMovement.cs
+++ Combat Arena/Assets/Scripts/PlayerMovement.cs
@@ -28,8 +28,11 @@ public class PlayerMovement : MonoBehaviour
             if (onGround == true)
             {
                 rb.AddForce(new Vector3(0, 1000, 0), ForceMode.Impulse);
+                onGround = false;
             }
         }
+            onGround = Physics.CheckBox(this.transform.position - new Vector3(0, 1.6f, 0), new Vector3(0.45f, 0.01f, 0.2f), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid"));
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             if (onGround == true)
