6584f0922cf913a680296b5fb7bff6ba2d40bf86
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2019-09-03 09:11:42 +0200

    Button Level Selector

---
+++ Combat Arena/Assets/Scripts Jordan/LevelChooser1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChooser1 : MonoBehaviour
+{
+    public void Process ()
+    {
+        LevelChooser.process = 1;
+    }
+
+    public void Process1 ()
+    {
+        LevelChooser.process = 0;
+    }
+}
