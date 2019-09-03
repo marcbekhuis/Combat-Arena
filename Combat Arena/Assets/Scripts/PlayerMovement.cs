0238e8ac5d5fc9e0dc59c3db6a6cb8b5bbe6919a
marc <marc@bekhuis.net>
2019-09-03 13:09:49 +0200

    jump fix

--- Combat Arena/Assets/Scripts/PlayerMovement.cs
+++ Combat Arena/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     public float speed = 5f;
     bool onGround = true;
+    float jumpCooldown = 0;
 
 
     // Start is called before the first frame update
@@ -25,23 +26,16 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = transform.rotation * new Vector3(Input.GetAxis(Xaxis) * speed, rb.velocity.y, Input.GetAxis(Yaxis) * speed);
 
-        if (Input.GetKey(jump))
+        if (Input.GetKey(jump) && jumpCooldown <= 0)
         {
             if (onGround == true)
             {
                 rb.AddForce(new Vector3(0, 35, 0), ForceMode.VelocityChange);
                 onGround = false;
+                jumpCooldown = 0.3f;
             }
         }
-            onGround = Physics.CheckBox(this.transform.position - new Vector3(0, 1.6f, 0), new Vector3(0.45f, 0.01f, 0.2f), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid"));
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            if (onGround == true)
-            {
-                rb.AddForce(new Vector3(0, -1000, 0), ForceMode.Impulse);
-            }
-        }
-
+        onGround = Physics.CheckBox(this.transform.position - new Vector3(0, 1.6f, 0), new Vector3(0.45f, 0.01f, 0.2f), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid"));
+        jumpCooldown -= Time.deltaTime;
     }
 }
