514a92c072b2f0cef537f6222d211ceba7c37657
remon66 <remonguus2002@gmail.com>
2019-09-04 10:01:20 +0200

    y7guhuio90ijhbiop0-[ih
    poiugfc

---
+++ Combat Arena/Assets/Scripts/VoidScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidScript : MonoBehaviour
+{
+    public HealthScript health;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ((health = other.gameObject.GetComponent<HealthScript>()) != null)
+        {
+            health.Gameover();
+        }
+    }
+}
