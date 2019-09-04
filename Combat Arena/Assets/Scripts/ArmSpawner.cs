1c7f6ea554e13b4c2ac946a4866c418aaaf0d6e4
marc <marc@bekhuis.net>
2019-09-04 11:00:16 +0200

    arm spawner upgrade 500

--- Combat Arena/Assets/Scripts/ArmSpawner.cs
+++ Combat Arena/Assets/Scripts/ArmSpawner.cs
@@ -38,4 +38,12 @@ public class ArmSpawner : MonoBehaviour
             }
         }
     }
+    void OnDrawGizmosSelected()
+    {
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(3, 3, 3));
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y, transform.position.z) - new Vector3(0, 5, 0), new Vector3(3, 7, 3));
+    }
 }
