be9ed9d4fc75e41b0db0a18a88f90f168c1a6874
marc <marc@bekhuis.net>
2019-09-05 11:48:05 +0200

    fixes 300

--- Combat Arena/Assets/Scripts/ArmSpawner.cs
+++ Combat Arena/Assets/Scripts/ArmSpawner.cs
@@ -9,12 +9,12 @@ public class ArmSpawner : MonoBehaviour
     public GameObject legendaryArm;
     [Space]
     public int amount = 10;
+    int armsSpawned = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        int y = 0;
-        for (int x = 0; x < amount && y < amount * 30; y++)
+        for (int y = 0; armsSpawned < amount && y < amount * 30; y++)
         {
             Vector3 location = new Vector3(Random.Range(-500, 500), transform.position.y, Random.Range(-500, 500));
             if (!Physics.CheckBox(location,new Vector3(1,1,1),new Quaternion(0,0,0,0), 1 << LayerMask.NameToLayer("Solid")) && Physics.CheckBox(location - new Vector3(0,4,0) , new Vector3(1, 3, 1), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid")) )
@@ -23,21 +23,49 @@ public class ArmSpawner : MonoBehaviour
                 if (randomInt < 60)
                 {
                     Instantiate(rareArm, location, new Quaternion(0, 0, 0, 0), this.transform);
-                    x++;
+                    armsSpawned++;
                 }
                 else if (randomInt < 85)
                 {
                     Instantiate(epicArm, location, new Quaternion(0, 0, 0, 0), this.transform);
-                    x++;
+                    armsSpawned++;
                 }
                 else if (randomInt < 100)
                 {
                     Instantiate(legendaryArm, location, new Quaternion(0, 0, 0, 0), this.transform);
-                    x++;
+                    armsSpawned++;
                 }
             }
         }
     }
+
+    private void Update()
+    {
+        if (armsSpawned < amount)
+        {
+            Vector3 location = new Vector3(Random.Range(-500, 500), transform.position.y, Random.Range(-500, 500));
+            if (!Physics.CheckBox(location, new Vector3(1, 1, 1), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid")) && Physics.CheckBox(location - new Vector3(0, 4, 0), new Vector3(1, 3, 1), new Quaternion(0, 0, 0, 0), 1 << LayerMask.NameToLayer("Solid")))
+            {
+                int randomInt = Random.Range(0, 100);
+                if (randomInt < 60)
+                {
+                    Instantiate(rareArm, location, new Quaternion(0, 0, 0, 0), this.transform);
+                    armsSpawned++;
+                }
+                else if (randomInt < 85)
+                {
+                    Instantiate(epicArm, location, new Quaternion(0, 0, 0, 0), this.transform);
+                    armsSpawned++;
+                }
+                else if (randomInt < 100)
+                {
+                    Instantiate(legendaryArm, location, new Quaternion(0, 0, 0, 0), this.transform);
+                    armsSpawned++;
+                }
+            }
+        }
+    }
+
     void OnDrawGizmosSelected()
     {
         // Draw a semitransparent blue cube at the transforms position
