2807599a47ce1271c35c0e88d7e3c61bae4f5663
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2019-09-10 09:09:35 +0200

    Tutorial Update

--- Combat Arena/Assets/Scripts/UI/TutorialScript1.cs
+++ Combat Arena/Assets/Scripts/UI/TutorialScript1.cs
@@ -13,16 +13,23 @@ public class TutorialScript1 : MonoBehaviour
     public GameObject text5;
     public GameObject text6;
     public GameObject text7;
+    public GameObject text8;
     public GameObject MenuButton;
     public GameObject NextButton;
 
     public void TutorialBack()
     {
         // makes certain text invisable and not appear until a certain text value has been given.
-        if (TutorialScript.text == 6)
+        if (TutorialScript.text == 7)
         {
             MenuButton.SetActive(false);
             NextButton.SetActive(true);
+            text8.SetActive(false);
+            TutorialScript.text = 6;
+            text7.SetActive(true);
+        }
+        if (TutorialScript.text == 6)
+        {
             text7.SetActive(false);
             TutorialScript.text = 5;
             text6.SetActive(true);
